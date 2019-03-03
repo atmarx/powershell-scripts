dd1fa9b7bea5aec775ea204d5b53462b8de8f362
atmarx <amarx@xram.net>
2019-03-03 18:01:48 -0500

    Update RetryFailedDPMJobs.ps1

--- RetryFailedDPMJobs.ps1
+++ RetryFailedDPMJobs.ps1
@@ -5,6 +5,8 @@
     Sleeps a minute in between checking jobs in progress
 .NOTES
     Thanks to https://charbelnemnom.com/2017/09/powershell-script-for-consistency-check-when-replica-is-inconsistent-dpm-scdpm-powershell/
+    for the inspiration.  Too bad my storage is too slow to just kick them all off and forget it -- I've found single file
+    to be more reliable.
 #>
 
 $DPMServername = $env:COMPUTERNAME
@@ -15,7 +17,7 @@ write-host "Found" $ReplicaErrors.length "errors"
 
 foreach ($ReplicaError in $ReplicaErrors) {
 
-    Write-Host "Next up:" $ReplicaError.TargetObjectName
+    Write-Host "Waiting to launch a consistency check for" $ReplicaError.TargetObjectName
 
     # Loop until any current jobs finish
     Do {
@@ -23,10 +25,10 @@ foreach ($ReplicaError in $ReplicaErrors) {
         $CurrentJobs = Get-DPMJob -Status InProgress
 
         if ($CurrentJobs.length -eq 0) {
-            Write-Host "Starting" $ReplicaError.TargetObjectName
+            Write-Host "Starting a consistency check for" $ReplicaError.TargetObjectName
             Start-DPMDatasourceConsistencyCheck -Datasource $ReplicaError.Datasource
         }
-        Write-Host "Job in progress, waiting a minute"
+        Write-Host "Found jobs already in progress -- going to sleep for a minute."
         Start-Sleep -s 60
 
     } While ($CurrentJobs.length -gt 0 )
